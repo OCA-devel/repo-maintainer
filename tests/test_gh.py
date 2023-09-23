847be6bc2fb10ac4272b6acd8d61b69fdc8d23c3
Simone Orsi <simahawk@gmail.com>
2023-09-23 12:21:02 +0200

    Sort files, rename tools and commands

--- tests/test_gh.py
+++ tests/test_gh.py
@@ -4,11 +4,12 @@ import unittest.mock
 
 import github3
 
-from oca_team_maintainer.generate import OcaTeamMaintainer
+from oca_repo_maintainer.tools.manager import RepoManager
 
 
 class TestGH(unittest.TestCase):
     def test_gh(self):
         print(os.getcwd())
         with unittest.mock.patch.object(github3, "login"):
-            OcaTeamMaintainer(os.getcwd() + "/conf", "dev", "ghp_123", True)()
+            manager = RepoManager(os.getcwd() + "/conf", "dev", "ghp_123", True)
+            manager.run()
